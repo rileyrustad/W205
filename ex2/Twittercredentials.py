05aff0f67da49a667bc12fe40c90ffe71182f3b7
Riley Rustad <rileyrustad@gmail.com>
2017-04-11 06:28:52 -0700

    added twitter credentials

---
+++ ex2/Twittercredentials.py
@@ -0,0 +1,23 @@
+import tweepy
+
+consumer_key = "blah";
+#eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
+
+
+consumer_secret = "blah";
+#eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+
+access_token = "blah-blah";
+#eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+
+access_token_secret = "blah";
+#eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+
